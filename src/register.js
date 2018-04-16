7ae7e89083e8a9faeaaa62de2b2816abb2ed2331
Pelle Braendgaard <pelle@stakeventures.com>
2018-04-16 17:18:06 -0600

    use ethjs instead of web3

--- src/register.js
+++ src/register.js
@@ -1,14 +1,15 @@
 import { registerMethod } from 'did-resolver'
-import Contract from 'truffle-contract'
+import HttpProvider from 'ethjs-provider-http'
+import Eth from 'ethjs-query'
+import abi from 'ethjs-abi'
+import EthContract from 'ethjs-contract'
 import DidRegistryContract from 'ethr-did-registry'
-import Web3 from 'web3'
 
 export const REGISTRY = '0xc1b66dea11f8f321b7981e1666fdaf3637fe0f61'
 
 export function wrapDidDocument (did, owner, history) {
   const now = Math.floor(new Date().getTime() / 1000)
   // const expired = {}
-  // console.log(history)
   const publicKey = [{
     id: `${did}#owner`,
     type: 'Secp256k1VerificationKey2018',
@@ -25,13 +26,13 @@ export function wrapDidDocument (did, owner, history) {
   const auth = {}
   const pks = {}
   const services = {}
-  for (let {event, args} of history) {
-    // console.log(`validTo: ${args.validTo.toNumber()} and now: ${now}`)
-    const key = `${event}-${args.delegateType||args.name}-${args.delegate||args.value}`
-    if (args.validTo && args.validTo.toNumber() >= now) {
-      if (event === 'DIDDelegateChanged') {      
+  for (let event of history) {
+    // console.log(`validTo: ${event.validTo.toNumber()} and now: ${now}`)
+    const key = `${event._eventName}-${event.delegateType||event.name}-${event.delegate||event.value}`
+    if (event.validTo && event.validTo.toNumber() >= now) {
+      if (event._eventName === 'DIDDelegateChanged') {      
         delegateCount++
-        switch (args.delegateType) {
+        switch (event.delegateType) {
           case 'Secp256k1SignatureAuthentication2018':
             auth[key] = {
               type: 'Secp256k1SignatureAuthentication2018',
@@ -42,12 +43,12 @@ export function wrapDidDocument (did, owner, history) {
               id: `${did}#delegate-${delegateCount}`,
               type: 'Secp256k1VerificationKey2018',
               owner: did,
-              ethereumAddress: args.delegate
+              ethereumAddress: event.delegate
             }
             break
         }
-      } else if (event === 'DIDAttributeChanged') {
-        const match = args.name.match(/^did\/(publicKey|authentication|service)\/(\w+)(\/(\w+))?$/)
+      } else if (event._eventName === 'DIDAttributeChanged') {
+        const match = event.name.match(/^did\/(publicKey|authentication|service)\/(\w+)(\/(\w+))?$/)
         if (match) {
           const section = match[1]
           const type = match[2]
@@ -64,27 +65,27 @@ export function wrapDidDocument (did, owner, history) {
                 case null:
                 case undefined:
                 case 'publicKeyHex':
-                  pk.publicKeyHex = args.value.slice(2)
+                  pk.publicKeyHex = event.value.slice(2)
                   break
                 case 'publicKeyBase64':
-                  pk.publicKeyBase64 = Buffer.from(args.value.slice(2), 'hex').toString('base64')
+                  pk.publicKeyBase64 = Buffer.from(event.value.slice(2), 'hex').toString('base64')
                   break
                 case 'publicKeyBase58':
-                  pk.publicKeyBase58 = Buffer.from(args.value.slice(2), 'hex').toString('base58')
+                  pk.publicKeyBase58 = Buffer.from(event.value.slice(2), 'hex').toString('base58')
                   break
                 default:
-                  pk.value = args.value
+                  pk.value = event.value
               }
               pks[key] = pk
               break
             case 'service':
-              services[key] = {type, serviceEndpoint: Buffer.from(args.value.slice(2), 'hex').toString()}
+              services[key] = {type, serviceEndpoint: Buffer.from(event.value.slice(2), 'hex').toString()}
               break
           }
         }
       }
     } else {
-      if ((event === 'DIDDelegateChanged' || (event === 'DIDAttributeChanged' && args.name.match(/^did\/publicKey\//))) && args.validTo.toNumber() === 0) delegateCount--
+      if ((event._eventName === 'DIDDelegateChanged' || (event._eventName === 'DIDAttributeChanged' && event.name.match(/^did\/publicKey\//))) && event.validTo.toNumber() === 0) delegateCount--
       delete auth[key]
       delete pks[key]
       delete services[key]
@@ -110,39 +111,34 @@ function configureProvider (conf = {}) {
   } else if (conf.web3) {
     return conf.web3.currentProvider
   } else {
-    return new Web3.providers.HttpProvider(conf.rpcUrl || 'https://mainnet.infura.io/ethr-did')
+    return new HttpProvider(conf.rpcUrl || 'https://mainnet.infura.io/ethr-did')
   }
 }
 
-export function configureRegistry (conf = {}) {
-  const registryAddress = conf.registry || REGISTRY
-  const DidReg = Contract(DidRegistryContract)
-  DidReg.setProvider(configureProvider(conf))
-  return DidReg.at(registryAddress)
-}
-
-function getLogs (filter) {
-  return new Promise((resolve, reject) => {
-    filter.get((error, events) => {
-      if (error) return reject(error)
-      resolve(events)
-    })
-  })
-}
-
 function register (conf = {}) {
-  const didReg = configureRegistry(conf)
+  const provider = configureProvider(conf)
+  const eth = new Eth(provider)
+  const registryAddress = conf.registry || REGISTRY
+  const DidReg = new EthContract(eth)(DidRegistryContract.abi)
+  const didReg = DidReg.at(registryAddress)
+  const logDecoder = abi.logDecoder(DidRegistryContract.abi, false)
 
+  const lastChanged = async (identity) => {
+    const result = await didReg.changed(identity)
+    if (result) {
+      return result['0']
+    }
+  }
   async function changeLog (identity) {
     const history = []
-    let previousChange = await didReg.changed(identity)
+    let previousChange = await lastChanged(identity)
     while (previousChange) {
-      const filter = await didReg.allEvents({topics: [identity], fromBlock: previousChange, toBlock: previousChange})
-      const events = await getLogs(filter)
+      const logs = await eth.getLogs({address: registryAddress, fromBlock: previousChange, toBlock: previousChange})
+      const events = logDecoder(logs)
       previousChange = undefined
       for (let event of events) {
         history.unshift(event)
-        previousChange = event.args.previousChange
+        previousChange = event.previousChange
       }
     }
     return history
@@ -151,7 +147,7 @@ function register (conf = {}) {
     if (!parsed.id.match(/^0x[0-9a-fA-F]{40}$/)) throw new Error(`Not a valid ethr DID: ${did}`)
     const owner = await didReg.identityOwner(parsed.id)
     const history = await changeLog(parsed.id)
-    return wrapDidDocument(did, owner, history)
+    return wrapDidDocument(did, owner['0'], history)
   }
   registerMethod('ethr', resolve)
 }
