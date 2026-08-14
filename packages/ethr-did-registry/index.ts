258ac5fcf308ed7a7838e1c6c0105fcc68e8fc5c
Mircea Nistor <mirceanis@gmail.com>
2026-08-14 10:24:55 +0300

    chore(registry): remove legacy files not part of monorepo adaptation

--- packages/ethr-did-registry/index.ts
+++
@@ -1,5 +0,0 @@
-// default deployments
-export { deployments, EthrDidRegistryDeployment } from './deployments'
-
-// contract ABI
-export { default as EthereumDIDRegistry } from './artifacts/contracts/EthereumDIDRegistry.sol/EthereumDIDRegistry.json'
